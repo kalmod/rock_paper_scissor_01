e25568f5591559a7e50d1e0735a0000faa8980cb
Kalmod <kalmod@live.com>
2023-02-21 19:48:50 -0500

    Added playRound function.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
 console.log('Hello, time to begin...')
 
 function getComputerChoice(){
-    const choices = ['Rock','Paper','Scissors']
+    const choices = ['rock','paper','scissors']
     let randomChoice = Math.floor(Math.random()*3)
     return choices[randomChoice]
 }
 
-for (let i = 0; i < 10; i++){
-    console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) return 'Draw';
+    else if (playerSelection === 'rock' && computerSelection === 'paper') return 'You Lose.';
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') return 'You Lose.';
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') return 'You Lose.';
+    else return 'You win!'
 }
+
+let playerChoice = (prompt('Please enter Rock, Paper, or Scissors.')).toLowerCase();
+let cpuChoice = getComputerChoice();
+
+console.log(`player: ${playerChoice} cpu: ${cpuChoice}`)
+console.log(playRound(playerChoice,cpuChoice))
