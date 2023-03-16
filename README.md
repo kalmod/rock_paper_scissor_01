dd999e71bddde7eb6431ff3028c1c5573da116b4
Kalmod <kalmod@live.com>
2023-03-15 20:02:13 -0400

    Updating CSS

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ The first iteration of the rock, paper, scissor project. No visual component yet
 3. create a `game` function. :heavy_check_mark:
     - This will call playround **5** times. 
     - keeps `score` and reports winner.
+
+# Gui Update
+
+Current Status: Made Cards and made Player choices hoverable.
+1. Add title to player and cpu choice.
+2. Add ScoreBoard.
+3. Add Reset button.
+4. Add animation to Cpu selection.
+5. Make cpu selection Hover and Red.
+6. Selected Player choice is green.
+
+RESOURCES
+[Convert string to Title Case with JavaScript](https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript)
+[Javascript Enums](https://masteringjs.io/tutorials/fundamentals/enum)
+[Make better RockPaperScissors Comparison](https://stackoverflow.com/questions/53730900/more-efficient-choice-comparison-for-rock-paper-scissors)
+[Fonts](https://stackoverflow.com/questions/33649957/how-to-set-the-font-family-like-a-digital-clock-css)
--- javascript.js
+++ javascript.js
@@ -1,28 +1,36 @@
 console.log('Hello, time to begin...')
 
 function getComputerChoice(){
-    const choices = ['rock','paper','scissors']
+    const choices = ['Rock','Paper','Scissors']
     let randomChoice = Math.floor(Math.random()*3)
     return choices[randomChoice]
 }
 
+const rpsObj = {
+    Rock:{weakTo: 'Paper', strongTo: 'Scissors'},
+    Paper:{weakTo: 'Scissors', strongTo: 'Rock'},
+    Scissors:{weakTo: 'Rock', strongTo: 'Paper'}
+}
+
+
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection) return 'Draw';
-    else if (playerSelection === 'rock' && computerSelection === 'paper') return 'You Lose.';
-    else if (playerSelection === 'paper' && computerSelection === 'scissors') return 'You Lose.';
-    else if (playerSelection === 'scissors' && computerSelection === 'rock') return 'You Lose.';
-    else return 'You win!'
+    if (rpsObj[playerSelection].weakTo === computerSelection) return 'You Lose.';
+    else if (rpsObj[playerSelection].strongTo === computerSelection) return 'You Win.';
+    else return 'DRAW!'
 }
 
 function choices(){
-    let playerChoice = (prompt('Please enter Rock, Paper, or Scissors.')).toLowerCase();
+    let playerChoice = (prompt('Please enter Rock, Paper, or Scissors.'));
+    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
     let cpuChoice = getComputerChoice();
     return { playerChoice, cpuChoice } ;
 }
+
+
 // 
 // console.log(playRound(playerChoice,cpuChoice))
-
+/*
 function game(rounds=5){
     let playerScore = 0;
     let cpuScore = 0;
@@ -30,15 +38,15 @@ function game(rounds=5){
         let { playerChoice, cpuChoice } = choices();
         let result = playRound(playerChoice, cpuChoice);
         console.log(`player: ${playerChoice} | cpu: ${cpuChoice} | result: ${result}`)
-        if (result === 'You win!'){
+        if (result === 'You Win.'){
             playerScore++;
         }
-        else if (result == 'You lose.'){
-            //cpuScore++;
+        else if (result == 'You Lose.'){
+            cpuScore++;
         }
         // (result === 'You win!') ? playerScore++ : (result === 'You win!') ? cpuScore++ : 0 ;
     }
-
+    console.log(`${playerScore} - ${cpuScore}`)
     if (playerScore > cpuScore) return 'Congratulations! You won!'
     else if (playerScore < cpuScore) return 'You lose! Good day Sir!'
     else return "Draw! Try again next time."
@@ -46,3 +54,17 @@ function game(rounds=5){
 }
 
 console.log(game());
+*/
+
+
+
+
+listOfPlayerChoices = [...document.querySelectorAll('#player-choices > .choice-container > .choice')];
+console.log(listOfPlayerChoices)
+listOfPlayerChoices.forEach((item,indx)=>{
+    item.addEventListener('click', e =>{
+        item.classList.add('playerChoice');
+    });
+});
+
+// console.log(document.querySelector("#player-choices"));
