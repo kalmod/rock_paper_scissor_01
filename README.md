832275809447aa3608c9dd1c3c4537ca5420b876
Kalmod <kalmod@live.com>
2023-03-27 21:01:43 -0400

    Added Choice Logic

--- README.md
+++ README.md
@@ -16,15 +16,18 @@ The first iteration of the rock, paper, scissor project. No visual component yet
 # Gui Update
 
 Current Status: Made Cards and made Player choices hoverable.
-1. Add title to player and cpu choice.
-2. Add ScoreBoard.
-3. Add Reset button.
+1. Add title to player and cpu choice. :heavy_check_mark:
+2. Add ScoreBoard. :heavy_check_mark:
+3. Add button. :heavy_check_mark:
+    - Play Button
+    - Reset Button
 4. Add animation to Cpu selection.
-5. Make cpu selection Hover and Red.
-6. Selected Player choice is green.
+5. Make cpu selection Hover and Red. :heavy_check_mark:
+6. Selected Player choice is green. :heavy_check_mark:
 
 RESOURCES
 [Convert string to Title Case with JavaScript](https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript)
 [Javascript Enums](https://masteringjs.io/tutorials/fundamentals/enum)
 [Make better RockPaperScissors Comparison](https://stackoverflow.com/questions/53730900/more-efficient-choice-comparison-for-rock-paper-scissors)
 [Fonts](https://stackoverflow.com/questions/33649957/how-to-set-the-font-family-like-a-digital-clock-css)
+[Interval to Stop Game](https://stackoverflow.com/questions/29173956/start-and-stop-loop-in-javascript-with-start-and-stop-button)
--- javascript.js
+++ javascript.js
@@ -1,10 +1,31 @@
 console.log('Hello, time to begin...')
 
+const listOfPlayerChoices = [...document.querySelectorAll('#player-choices > .choice-container > .choice')];
+const listOfCpuChoices = [...document.querySelectorAll('#cpu-choices > .choice-container > .choice')];
+
+const playButton = document.querySelector('#playButton');
+const resetButton = document.querySelector('#resetButton');
+const scoreBoard = document.querySelector('#current-score');
+
+let playState = 0;
+let playerChoiceMade = false, cpuChoiceMade = false;
+// function getComputerChoice(){
+//     const choices = ['Rock','Paper','Scissors']
+//     let randomChoice = Math.floor(Math.random()*3)
+//     return choices[randomChoice]
+// }
+
+
 function getComputerChoice(){
     const choices = ['Rock','Paper','Scissors']
     let randomChoice = Math.floor(Math.random()*3)
-    return choices[randomChoice]
+    console.log(randomChoice)
+    let cpuChoice = document.querySelector(`#cpu-choices [data-choice="${choices[randomChoice]}"]`);
+    cpuChoice.classList.add('cpuChoice');
+    cpuChoice.style.setProperty('--cpu-inactive-color','transparent')
+    return cpuChoice.dataset.choice
 }
+
 
 const rpsObj = {
     Rock:{weakTo: 'Paper', strongTo: 'Scissors'},
@@ -20,12 +41,21 @@ function playRound(playerSelection, computerSelection){
     else return 'DRAW!'
 }
 
-function choices(){
-    let playerChoice = (prompt('Please enter Rock, Paper, or Scissors.'));
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
-    let cpuChoice = getComputerChoice();
-    return { playerChoice, cpuChoice } ;
-}
+// function choices(){
+//     let playerChoice = (prompt('Please enter Rock, Paper, or Scissors.'));
+//     playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
+//     let cpuChoice = getComputerChoice();
+//     return { playerChoice, cpuChoice } ;
+// }
+
+// function choices(){
+//     let chosen = (document.querySelector('.playerChoice')).dataset.choice
+//     let playerChoice = chosen.dataset.choice
+//     if (chosen){
+//         let cpuChoice = getComputerChoice();
+//     }
+//     return { playerChoice, cpuChoice } ;
+// }
 
 
 // 
@@ -52,19 +82,60 @@ function game(rounds=5){
     else return "Draw! Try again next time."
 
 }
-
-console.log(game());
 */
+// console.log(game());
+
+function game(){
+    let playerChoice, cpuChoice;
+    if (!playerChoiceMade){
+        console.log('pchoice')
+        playerChoice = (document.querySelector('.playerChoice')).dataset.choice
+        playerChoiceMade = true;
+    }
+    console.log(playerChoice, cpuChoice)
+}
+
+function removeChoices(playerChoices, cpuChoices){
+    playerChoices.forEach((item,indx) => {
+        item.classList.remove('playerChoice')
+    })
+    cpuChoices.forEach((item,indx) => {
+        item.classList.remove('cpuChoice')
+        item.style.setProperty('--cpu-inactive-color','rgba(10,10,10,0.4)')
+    })
+
+}
 
 
-
-
-listOfPlayerChoices = [...document.querySelectorAll('#player-choices > .choice-container > .choice')];
 console.log(listOfPlayerChoices)
+
+playButton.addEventListener('click',function (e){
+    if (playState) {
+        playState = 0;
+        console.log('Stop');
+        playButton.textContent = 'Play';
+        removeChoices(listOfPlayerChoices,listOfCpuChoices)
+    }
+    else {
+        playState = 1;
+        playButton.textContent = 'Stop';
+        console.log('Start');
+
+    }
+    
+})
+
 listOfPlayerChoices.forEach((item,indx)=>{
     item.addEventListener('click', e =>{
-        item.classList.add('playerChoice');
+        if (playState) {
+            item.classList.add('playerChoice');
+            let playerChoice = (document.querySelector('.playerChoice')).dataset.choice
+            let cpuChoice = getComputerChoice();
+            console.log(playerChoice,cpuChoice)
+        }
     });
 });
+
+
 
 // console.log(document.querySelector("#player-choices"));
